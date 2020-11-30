a23db6bd293af1d670e9a75d86bed60fb587bdac
Arnar Már <arnarmar9@hotmail.com>
2020-11-30 13:49:11 +0000

    beginning with contracts class

---
+++ src/Models/contracts_class.py
@@ -0,0 +1,16 @@
+class Contracts:
+    def __init__(self, name, phone, address, email, date_from, date_to, vehicle_id, location, vehicle_status, employee_id, loan_date, return_date, total, loan_status):
+        self.name = name
+        self.phone = phone
+        self.address = address
+        self.email = email
+        self.date_from = date_from
+        self.date_to = date_to
+        self.vehicle_id = vehicle_id
+        self.location = location
+        self.vehicle_status = vehicle_status
+        self.employee_id = employee_id
+        self.loan_date = loan_date
+        self.return_date = return_date
+        self.total = total
+        self.loan_status = loan_status
