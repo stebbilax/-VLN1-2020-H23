b0f22ee1491d2acce9b7ddbaa53cadf8e430f9b8
Gunnlaug margret <gunnlaug97@gmail.com>
2020-12-03 15:03:02 +0000

    Vehicle operations

--- src/Presentation/Operations.py
+++ src/Presentation/Operations.py
@@ -1,6 +1,6 @@
 def test(logicAPI, ui):
     print(ui.get_user_form(['SSID', 'TYPE', 'COLOR']))
-
+#employees
 def display_all_employees(logicAPI, ui):
     for employee in logicAPI.employee.get_all_employees():
         print(employee)
@@ -10,3 +10,25 @@ def display_all_employees(logicAPI, ui):
 def display_all_contracts(logicAPI, ui):
     for contract in logicAPI.contract.get_all_contracts():
         print(contract)
+
+#vehicles
+
+def display_all_vehicles(logicAPI, ui):
+    for vehicles in logicAPI.vehicles.get_all_vehicles():
+        print(vehicles)
+
+def display_all_vehicles_in_a_location(logicAPI,ui):
+    pass
+
+def display_vehicle_condition(logicAPI,ui):
+    pass
+
+
+def register_vehicle(logicAPI,ui):
+    #must include vehicle authentication
+    #must include vehicle condition
+    pass
+
+def  edit_veichle(logicAPI,ui):
+    #serch for vehicle to be able to edit
+    pass
