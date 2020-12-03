1d0857887c8f9aecd6b15954bbc8f09c80bf24fe
Zatrac <eliasf_96@hotmail.com>
2020-12-03 13:24:20 +0000

    Search API extended to logic API

--- src/Logic/LogicAPI.py
+++ src/Logic/LogicAPI.py
@@ -1,15 +1,18 @@
 from Data.DataAPI import DataAPI
+from Logic.Search_API import Search_API
 
 class LogicAPI:
     def __init__(self):
         self.dataAPI = DataAPI()
-        self.vehicles = ManageVehicles(self.dataAPI)
-        self.employee = ManageEmployees(self.dataAPI)
-        self.contract = ManageContracts(self.dataAPI)
+        self.searchAPI = Search_API()
+        self.vehicles = ManageVehicles(self.dataAPI, self.searchAPI)
+        self.employee = ManageEmployees(self.dataAPI, self.searchAPI)
+        self.contract = ManageContracts(self.dataAPI, self.searchAPI)
 
 class ManageVehicles:
-    def __init__(self, api):
-        self.dataAPI = api
+    def __init__(self, dapi, sapi):
+        self.dataAPI = dapi
+        self.searchAPI = sapi
 
     def register_vehicle(self):
         pass
@@ -17,41 +20,43 @@ class ManageVehicles:
     def edit_vehicle(self):
         pass
 
-    def get_vehicle(self):
+    def get_vehicle(self, field, value):
         pass # Search
 
     def get_all_vehicles(self):
         return self.dataAPI.read_all_vehicles()
 
 class ManageEmployees:
-    def __init__(self, api):
-        self.dataAPI = api
+    def __init__(self, dapi, sapi):
+        self.dataAPI = dapi
+        self.searchAPI = sapi
     
-    def register_employee():
+    def register_employee(self):
         pass
     
-    def edit_employee():
+    def edit_employee(self):
         pass
     
-    def get_employee():
+    def get_employee(self):
         pass # Search
     
-    def get_all_employees():
+    def get_all_employees(self):
         return self.dataAPI.read_all_employees()
     
 
 class ManageContracts:
-    def __init__(self, api):
-        self.dataAPI = api
+    def __init__(self, dapi, sapi):
+        self.dataAPI = dapi
+        self.searchAPI = sapi
     
-    def register_contract():
+    def register_contract(self):
         pass
     
-    def edit_contract():
+    def edit_contract(self):
         pass
     
-    def get_contract():
+    def get_contract(self):
         pass # Search
     
-    def get_all_contracts():
+    def get_all_contracts(self):
         return self.dataAPI.read_all_contracts()
