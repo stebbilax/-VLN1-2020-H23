575e7439e5c4c9f2c0afa0779904acdff0bc75b3
Stefán Laxdal <stebbilax@gmail.com>
2020-12-03 12:01:17 +0000

    Added enums class

---
+++ src/Models/Enums.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+class Enum_Airport(Enum):
+    # Airports
+    reykjavik = 1
+    nuuk = 2
+    kulusk = 3
+    tingwall = 4
+    longyearbyen = 5
+    torshavn = 6
+
+
+class Enum_Title(Enum):
+    #Titles 
+    office = 1
+    airport = 2
+
+
+class Enum_Country(Enum):
+    # Countries
+    iceland = 1
+    greenland = 2
+    shetland = 3
+    svalbard = 4
+    faroe_islands = 5
