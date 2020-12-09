080b1bcba537c33186424c56badca86eabfa5bde
Stefán Laxdal <stebbilax@gmail.com>
2020-12-09 14:26:39 +0000

    added form calculators in logic

--- src/Logic/Report_Generator.py
+++ src/Logic/Report_Generator.py
@@ -1,4 +1,5 @@
 from datetime import datetime, date
+from Logic.form_calculators import calculate_late_fee
 
 class Report_Generator:
     def __init__(self, dapi, sapi):
@@ -10,29 +11,6 @@ class Report_Generator:
         contracts = [vars(contract) for contract in self.Data.read_all_contracts()]
         report = {}
 
-        def calculate_late_fee(rate, contract_end, date_return):
-            ''' Calculates late fee
-            For every given contract, adds 20% of the rate on top of the original rate for every
-            day that has passed the contract_end date. If vehicle has not been returned
-            the function returns late fee as it would be as of date of calculation '''
-            
-            try:
-                d1 = datetime.fromisoformat(contract_end)
-                d2 = datetime.fromisoformat(date_return)
-                days_late = (d2-d1).days
-
-                if days_late < 1 : return 0
-
-                return (days_late * int(rate)) + ((days_late * int(rate)) * 0.2)
-
-            except ValueError:
-                d1 = datetime.fromisoformat(contract_end)
-                d2 = datetime.today()
-                days_late = (d2-d1).days
-
-                if days_late < 1 : return 0
-
-                return (days_late * int(rate)) + ((days_late * int(rate)) * 0.2)
 
         def select_time_period(contracts, date_from, date_to):
             '''Recieves contracts and a start and end date.
