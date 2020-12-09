4a143244b0001e6d53f45eb5d061e75d38601149
Gunnlaug margret <gunnlaug97@gmail.com>
2020-12-09 15:30:44 +0000

    working on form fillers

--- src/Logic/form_editors.py
+++ src/Logic/form_editors.py
@@ -1,7 +1,13 @@
-def vehicle_editor(form):
+from
+
+vehicle_editor(form):
 
     # make sure no vehicle is defective and available
     if form['vehicle_state'] == 'DEFECTIVE': form['vehicle_status'] = 'Unavailable'
+    if form['contract_start'] == '2020-11-01':if form['contract_end'] == '2021-03-31':
+        if ['vehicle_type'] == 'Light water' or ['vehicle_type'] = 'Light road':
+            form['late_fee'] = '0'
+            form['date_return'] = 
 
 
     return form
